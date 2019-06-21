dc03d169a5c02bce76ecb9628a5e116fddecbf15
el-cid <goomizar@gmail.com>
2019-06-21 12:57:58 -0500

    1.2: course information, step2

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts.part1} exercises={props.exercises.exercises1} />
+      <Part part={props.parts.part2} exercises={props.exercises.exercises2} />
+      <Part part={props.parts.part3} exercises={props.exercises.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.number}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const parts = { part1, part2, part3 };
+  const exercises = { exercises1, exercises2, exercises3 };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total number={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
